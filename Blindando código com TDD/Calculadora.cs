120b4d4506235f36320956c6b40257be90b5b2bb
Antonio-A-N-J <antonionunes710@gmail.com>
2024-11-15 23:12:47 -0300

    Adicionar arquivos de projeto.

---
+++ Blindando código com TDD/Calculadora.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blindando_código_com_TDD
+{
+	public class Calculadora
+	{
+		private List<string> listaHistorico;
+
+		public Calculadora()
+		{
+			listaHistorico = new List<string>();
+		}
+		public int somar(int val1, int val2)
+		{
+			int res = val1 + val2;
+			return res;
+		}
+
+		public int subtrair(int val1, int val2)
+		{
+			int res = val1 - val2;
+			listaHistorico.Insert(0, "Res" + res);
+			return res;
+		}
+
+		public int multiplicar(int val1, int val2)
+		{
+			int res = val1 * val2;
+			listaHistorico.Insert(0, "Res" + res);
+			return res;
+		}
+
+		public int dividir(int val1, int val2)
+		{
+			int res = val1 / val2;
+			listaHistorico.Insert(0, "Res" + res);
+			return res;
+		}
+
+		public List<string> historico()
+		{
+			listaHistorico.RemoveRange(3, listaHistorico.Count -2);
+			return listaHistorico;
+		}
+	}
+}
